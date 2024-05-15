0b97ecd2400b6198b3e36208200dece751ec2018
Justin Eskesen <jeskesen@chanzuckerberg.com>
2024-05-15 18:58:18 +0000

    arbitrary stopping point for mini code review w/ Alan

--- include/acquire-zarr/acquire-zarr.h
+++ include/acquire-zarr/acquire-zarr.h
@@ -9,32 +9,41 @@
 #define EXTERNC
 #endif
 
-// Forward declaration of ZarrSink
-struct ZarrSink;
+// Forward declaration of AcquireZarrSinkWrapper
+struct AcquireZarrSinkWrapper;
 
 // Compression options for Zarr
 enum AcquireZarrCompression
 {
     AcquireZarrCompression_NONE = 0,
-    AcquireZarrCompression_ZSTD,
     AcquireZarrCompression_BLOSC
-} ;
+};
+
+// Zarr Version
+enum AcquireZarrVersion
+{
+    AcquireZarrVersion_2 = 2,
+    AcquireZarrVersion_3 = 3
+};
 
 // Configuration for Zarr sink
-struct ZarrSinkConfig
+struct AcquireZarrSinkConfig
 {
     char filename[512];
     enum AcquireZarrCompression compression;
     int multiscale; // 0 or 1, because I don't have bool in C
-} ;
+};
+
+
+// 
 
 // Open a Zarr sink with the given configuration
-EXTERNC struct ZarrSink* zarr_sink_open(const struct ZarrSinkConfig* config);
+EXTERNC struct AcquireZarrSinkWrapper* zarr_sink_open(const struct AcquireZarrSinkConfig* config);
 
 // Close the Zarr sink
 EXTERNC void zarr_sink_close();
 
-EXTERNC int zarr_sink_append(struct ZarrSink*, uint8_t* image_data, uint8_t dimensions, uint16_t shape);
+EXTERNC int zarr_sink_append(struct AcquireZarrSinkWrapper*, uint8_t* image_data, uint8_t dimensions, uint16_t shape);
 
 
 #endif // ACQUIRE_ZARR_H
--- src/api.cpp
+++ src/api.cpp
@@ -1,11 +1,13 @@
 #include "acquire-zarr/acquire-zarr.h"
-#include "zarr.hh"
+#include "zarr.v2.hh"
 
 
 
-EXTERNC struct ZarrSink* zarr_sink_open(const struct ZarrSinkConfig* config)
+EXTERNC struct AcquireZarrSinkWrapper* zarr_sink_open(const struct AcquireZarrSinkConfig* config)
 {
-    struct ZarrSink* myptr = 0;
+    struct AcquireZarrSinkWrapper* myptr = new AcquireZarrSinkWrapper(config);
+    
+    //myptr->open();
     return myptr;
     
 }
@@ -15,26 +17,59 @@ EXTERNC void zarr_sink_close()
 
 }
 
-EXTERNC int zarr_sink_append(struct ZarrSink*, uint8_t* image_data, uint8_t dimensions, uint16_t shape)
+EXTERNC int zarr_sink_append(struct AcquireZarrSinkWrapper*, uint8_t* image_data, uint8_t dimensions, uint16_t shape)
 {
 }
 
 
 
 // C++ implementation of the ZarrSink, which is just a wrapper around the Zarr storage class heirarchy.
-struct ZarrSink 
-{
-    ZarrSink(const struct ZarrSinkConfig* config);
-    ~ZarrSink();
+using namespace acquire::sink::zarr;
 
-    ZarrSinkConfig config_;
-    std::shared_ptr<struct Zarr> zarr_ = nullptr;
+struct AcquireZarrSinkWrapper 
+{
+  public:
+    AcquireZarrSinkWrapper(const struct AcquireZarrSinkConfig* config);
+    ~AcquireZarrSinkWrapper();
+
+    int open();
+  protected:
+    AcquireZarrSinkConfig config_;
+    std::shared_ptr<struct Zarr> zarr_sink_ = nullptr;
 };
 
-ZarrSink::ZarrSink(const struct ZarrSinkConfig* config)
+AcquireZarrSinkWrapper::AcquireZarrSinkWrapper(const struct AcquireZarrSinkConfig* config)
 {
-    if(std::memcpy(&config_, config, sizeof(ZarrSinkConfig)) != &config_)
+    config_ = *config;
+
+    if (config_.compression != AcquireZarrCompression_NONE)
     {
-        throw std::runtime_error("Failed to copy ZarrSinkConfig");
+        // Set the compression parameters
+        BloscCompressionParams blosc_params;
+        blosc_params.codec_id = (config_.compression == AcquireZarrCompression_BLOSC_LZ4) ? 
+            compression_codec_as_string< BloscCodecId::Lz4 >() : 
+            compression_codec_as_string< BloscCodecId::Zstd >();
+
+        blosc_params.clevel = 1;
+        blosc_params.shuffle = 1;
+        zarr_sink_ = std::make_shared<Zarr>(blosc_params);
+
     }
+    else
+    {
+        // No compression
+        zarr_sink_ = std::make_shared<Zarr>();
+    }
+}
+
+
+AcquireZarrSinkWrapper::~AcquireZarrSinkWrapper()
+{
+}
+
+int AcquireZarrSinkWrapper::open()
+{
+
+    zarr_ = std::make_shared<Zarr>(config_.filename);
+    return 0;
 }
