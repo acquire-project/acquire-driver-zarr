c593a4df24faab61a8a33ce588d70884bea44169
Alan Liddell <aliddell@chanzuckerberg.com>
2024-05-28 10:50:54 -0400

    Force dynamically linking the MSVC runtime.

--- cmake/msvc.cmake
+++
@@ -1,7 +0,0 @@
-option(ACQUIRE_MSVC_USE_MD "When ON, prefer linking to the Multithreaded DLL Microsoft C Runtime library (use /MD)")
-
-if(ACQUIRE_MSVC_USE_MD)
-    set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>DLL")
-else()
-    set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
-endif()
