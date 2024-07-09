9104c252db73b99a1f66fd141589183b282c15a0
Justin Eskesen <jeskesen@chanzuckerberg.com>
2024-07-09 09:13:09 -0700

    remove old commented-out config data-structure

--- api/cpp/include/acquire-zarr/acquire-zarr.hh
+++ api/cpp/include/acquire-zarr/acquire-zarr.hh
@@ -3,48 +3,7 @@
 
 #include <memory>
 #include <vector>
-/*
-// Configuration for Zarr sink
-struct AcquireZarrSinkConfig
-{
-    char filename[512];
 
-    struct shape_s
-    {
-        uint32_t channels, width, height, planes;
-    } shape;
-
-    // 8, 12, 14, 16, 32  (non power of 2 values are rounded up to the next power of 2)
-    uint8_t sample_bits;  
-
-    enum SampleType
-    {
-        UNKNOWN,
-        UNSIGNED_INT,
-        SIGNED_INT,
-        FLOAT
-    } word_type;    
-
-    // Zarr Version
-    enum AcquireZarrVersion
-    {
-        AcquireZarrVersion_2,
-        AcquireZarrVersion_3
-    } zarr_version;
-
-    // Compression options for Zarr
-    enum AcquireZarrCompression
-    {
-        AcquireZarrCompression_NONE,
-        AcquireZarrCompression_BLOSC_LZ4,
-        AcquireZarrCompression_BLOSC_ZSTD,
-    } compression;
-
-    uint8_t multiscale; // 0 or 1, because I don't have bool in C
-};
-
-
-*/
 
 enum class AcquireZarrCompressionCodec
 {
