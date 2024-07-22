00c3e9a6852145b48b5d61ca85ec48740eeaf181
Justin Eskesen <jeskesen@chanzuckerberg.com>
2024-07-22 08:56:03 -0700

    added chunk_size to python api test

--- api/python/tests/test_zarr.py
+++ api/python/tests/test_zarr.py
@@ -5,7 +5,7 @@ import numpy as np
 import acquire_zarr
 from time import sleep
 
-data = np.arange(64, 64, 3, dtype=np.uint8)
+data = np.arange(64, 64, dtype=np.uint8)
 
 def check_zarr(v3: bool, uri: str) -> None:
     zarr = acquire_zarr.AcquireZarrWriter()
@@ -19,21 +19,26 @@ def check_zarr(v3: bool, uri: str) -> None:
     zarr.dimension_sizes = [64, 64, 0]
     assert zarr.dimension_sizes == [64, 64, 0]
     
+    zarr.chunk_sizes = [64, 64, 1]
+    zarr.chunk_sizes[-1] = 1
+    assert zarr.chunk_sizes == [64, 64, 1]
+    
     zarr.compression_codec = acquire_zarr.CompressionCodec.COMPRESSION_NONE
     assert zarr.compression_codec == acquire_zarr.CompressionCodec.COMPRESSION_NONE
     
-    zarr.compression_codec = acquire_zarr.CompressionCodec.COMPRESSION_BLOSC_ZSTD
-    assert zarr.compression_codec == acquire_zarr.CompressionCodec.COMPRESSION_BLOSC_ZSTD
+    #zarr.compression_codec = acquire_zarr.CompressionCodec.COMPRESSION_BLOSC_ZSTD
+    #assert zarr.compression_codec == acquire_zarr.CompressionCodec.COMPRESSION_BLOSC_ZSTD
     
     zarr.compression_level = 5
     assert zarr.compression_level == 5
     zarr.compression_shuffle = 0
     assert zarr.compression_shuffle == 0
     
+    sleep(1)
     zarr.start()
     for i in range(3):
         zarr.append(data)
-        #sleep(0.5)
+        sleep(0.5)
         
 
 def test_zarr_v2() -> None:
