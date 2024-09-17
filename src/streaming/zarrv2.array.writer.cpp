d291c537c6f6bb8e1aef013a6995ef728673fb33
Alan Liddell <aliddell@chanzuckerberg.com>
2024-09-17 16:59:15 -0400

    Implement and test ZarrV2ArrayWriter.

---
+++ src/streaming/zarrv2.array.writer.cpp
@@ -0,0 +1,177 @@
+#include "macros.hh"
+#include "zarrv2.array.writer.hh"
+#include "sink.creator.hh"
+#include "zarr.common.hh"
+
+#include <nlohmann/json.hpp>
+
+#include <latch>
+#include <stdexcept>
+
+namespace {
+std::string
+sample_type_to_dtype(ZarrDataType t)
+
+{
+    const std::string dtype_prefix =
+      std::endian::native == std::endian::big ? ">" : "<";
+
+    switch (t) {
+        case ZarrDataType_uint8:
+            return dtype_prefix + "u1";
+        case ZarrDataType_uint16:
+            return dtype_prefix + "u2";
+        case ZarrDataType_uint32:
+            return dtype_prefix + "u4";
+        case ZarrDataType_uint64:
+            return dtype_prefix + "u8";
+        case ZarrDataType_int8:
+            return dtype_prefix + "i1";
+        case ZarrDataType_int16:
+            return dtype_prefix + "i2";
+        case ZarrDataType_int32:
+            return dtype_prefix + "i4";
+        case ZarrDataType_int64:
+            return dtype_prefix + "i8";
+        case ZarrDataType_float32:
+            return dtype_prefix + "f4";
+        case ZarrDataType_float64:
+            return dtype_prefix + "f8";
+        default:
+            throw std::runtime_error("Invalid data type: " +
+                                     std::to_string(static_cast<int>(t)));
+    }
+}
+} // namespace
+
+zarr::ZarrV2ArrayWriter::ZarrV2ArrayWriter(
+  const ArrayWriterConfig& config,
+  std::shared_ptr<ThreadPool> thread_pool,
+  std::shared_ptr<S3ConnectionPool> s3_connection_pool)
+  : ArrayWriter(config, thread_pool, s3_connection_pool)
+{
+}
+
+zarr::ZarrV2ArrayWriter::~ZarrV2ArrayWriter()
+{
+    is_finalizing_ = true;
+    try {
+        flush_();
+    } catch (const std::exception& exc) {
+        LOG_ERROR("Failed to finalize array writer: %s", exc.what());
+    } catch (...) {
+        LOG_ERROR("Failed to finalize array writer: (unknown)");
+    }
+}
+
+ZarrVersion
+zarr::ZarrV2ArrayWriter::version_() const
+{
+    return ZarrVersion_2;
+}
+
+bool
+zarr::ZarrV2ArrayWriter::flush_impl_()
+{
+    // create chunk files
+    CHECK(data_sinks_.empty());
+    if (!make_data_sinks_()) {
+        return false;
+    }
+
+    CHECK(data_sinks_.size() == chunk_buffers_.size());
+
+    std::latch latch(chunk_buffers_.size());
+    {
+        std::scoped_lock lock(buffers_mutex_);
+        for (auto i = 0; i < data_sinks_.size(); ++i) {
+            auto& chunk = chunk_buffers_.at(i);
+            EXPECT(thread_pool_->push_to_job_queue(
+                     std::move([&sink = data_sinks_.at(i),
+                                data = chunk.data(),
+                                size = chunk.size(),
+                                &latch](std::string& err) -> bool {
+                         bool success = false;
+                         try {
+                             CHECK(sink->write(0, data, size));
+                             success = true;
+                         } catch (const std::exception& exc) {
+                             err = "Failed to write chunk: " +
+                                   std::string(exc.what());
+                         } catch (...) {
+                             err = "Failed to write chunk: (unknown)";
+                         }
+
+                         latch.count_down();
+                         return success;
+                     })),
+                   "Failed to push job to thread pool");
+        }
+    }
+
+    // wait for all threads to finish
+    latch.wait();
+
+    return true;
+}
+
+bool
+zarr::ZarrV2ArrayWriter::write_array_metadata_()
+{
+    if (!make_metadata_sink_()) {
+        return false;
+    }
+
+    using json = nlohmann::json;
+
+    std::vector<size_t> array_shape, chunk_shape;
+
+    size_t append_size = frames_written_;
+    for (auto dim = config_.dimensions.rbegin() + 2;
+         dim < config_.dimensions.rend() - 1;
+         ++dim) {
+        CHECK(dim->array_size_px);
+        append_size =
+          (append_size + dim->array_size_px - 1) / dim->array_size_px;
+    }
+    array_shape.push_back(append_size);
+
+    chunk_shape.push_back(config_.dimensions.front().chunk_size_px);
+    for (auto dim = config_.dimensions.begin() + 1;
+         dim != config_.dimensions.end();
+         ++dim) {
+        array_shape.push_back(dim->array_size_px);
+        chunk_shape.push_back(dim->chunk_size_px);
+    }
+
+    json metadata;
+    metadata["zarr_format"] = 2;
+    metadata["shape"] = array_shape;
+    metadata["chunks"] = chunk_shape;
+    metadata["dtype"] = sample_type_to_dtype(config_.dtype);
+    metadata["fill_value"] = 0;
+    metadata["order"] = "C";
+    metadata["filters"] = nullptr;
+    metadata["dimension_separator"] = "/";
+
+    if (config_.compression_params) {
+        const BloscCompressionParams bcp = *config_.compression_params;
+        metadata["compressor"] = json{ { "id", "blosc" },
+                                       { "cname", bcp.codec_id },
+                                       { "clevel", bcp.clevel },
+                                       { "shuffle", bcp.shuffle } };
+    } else {
+        metadata["compressor"] = nullptr;
+    }
+
+    const std::string metadata_str = metadata.dump(4);
+    const auto* metadata_bytes = (const uint8_t*)metadata_str.c_str();
+
+    return metadata_sink_->write(0, metadata_bytes, metadata_str.size());
+}
+
+bool
+zarr::ZarrV2ArrayWriter::should_rollover_() const
+{
+    return true;
+}
---
+++ tests/unit-tests/zarrv2-writer-write-ragged-append-dim.cpp
@@ -0,0 +1,151 @@
+#include "zarrv2.array.writer.hh"
+#include "macros.hh"
+#include "zarr.common.hh"
+
+#include <nlohmann/json.hpp>
+#include <filesystem>
+
+#define EXPECT_EQ(a, b)                                                        \
+    EXPECT((a) == (b), "Expected %s == %s, but %zu != %zu", #a, #b, a, b)
+
+namespace fs = std::filesystem;
+
+namespace {
+const fs::path base_dir = fs::temp_directory_path() / TEST;
+
+const unsigned int array_width = 64, array_height = 48, array_planes = 5;
+const unsigned int n_frames = array_planes;
+
+const unsigned int chunk_width = 16, chunk_height = 16, chunk_planes = 2;
+
+const unsigned int chunks_in_x =
+  (array_width + chunk_width - 1) / chunk_width; // 4 chunks
+const unsigned int chunks_in_y =
+  (array_height + chunk_height - 1) / chunk_height; // 3 chunks
+
+const unsigned int chunks_in_z =
+  (array_planes + chunk_planes - 1) / chunk_planes; // 3 chunks, ragged
+
+const int level_of_detail = 1;
+} // namespace
+
+void
+check_json()
+{
+    fs::path zarray_path =
+      base_dir / std::to_string(level_of_detail) / ".zarray";
+    CHECK(fs::is_regular_file(zarray_path));
+
+    std::ifstream f(zarray_path);
+    nlohmann::json zarray = nlohmann::json::parse(f);
+
+    EXPECT(zarray["dtype"].get<std::string>() == "<u1",
+           "Expected dtype to be '<u1', but got '%s'",
+           zarray["dtype"].get<std::string>().c_str());
+
+    EXPECT_EQ(zarray["zarr_format"].get<int>(), 2);
+
+    const auto& chunks = zarray["chunks"];
+    EXPECT_EQ(chunks.size(), 3);
+    EXPECT_EQ(chunks[0].get<int>(), chunk_planes);
+    EXPECT_EQ(chunks[1].get<int>(), chunk_height);
+    EXPECT_EQ(chunks[2].get<int>(), chunk_width);
+
+    const auto& shape = zarray["shape"];
+    EXPECT_EQ(shape.size(), 3);
+    EXPECT_EQ(shape[0].get<int>(), array_planes);
+    EXPECT_EQ(shape[1].get<int>(), array_height);
+    EXPECT_EQ(shape[2].get<int>(), array_width);
+}
+
+int
+main()
+{
+    Logger::set_log_level(ZarrLogLevel_Debug);
+
+    int retval = 1;
+
+    const ZarrDataType dtype = ZarrDataType_uint8;
+    const unsigned int nbytes_px = zarr::bytes_of_type(dtype);
+
+    try {
+        auto thread_pool = std::make_shared<zarr::ThreadPool>(
+          std::thread::hardware_concurrency(), [](const std::string& err) {
+              LOG_ERROR("Error: %s\n", err.c_str());
+          });
+
+        std::vector<zarr::Dimension> dims;
+        dims.emplace_back(
+          "z", ZarrDimensionType_Space, array_planes, chunk_planes, 0);
+        dims.emplace_back(
+          "y", ZarrDimensionType_Space, array_height, chunk_height, 0);
+        dims.emplace_back(
+          "x", ZarrDimensionType_Space, array_width, chunk_width, 0);
+
+        zarr::ArrayWriterConfig config = {
+            .dimensions = dims,
+            .dtype = dtype,
+            .level_of_detail = level_of_detail,
+            .bucket_name = std::nullopt,
+            .store_path = base_dir.string(),
+            .compression_params = std::nullopt,
+        };
+
+        {
+            zarr::ZarrV2ArrayWriter writer(config, thread_pool, nullptr);
+
+            const size_t frame_size = array_width * array_height * nbytes_px;
+            std::vector<uint8_t> data(frame_size, 0);
+
+            for (auto i = 0; i < n_frames; ++i) { // 2 time points
+                CHECK(writer.write_frame(data.data(), frame_size));
+            }
+        }
+
+        check_json();
+
+        const auto expected_file_size =
+          chunk_width * chunk_height * chunk_planes * nbytes_px;
+
+        const fs::path data_root =
+          base_dir / std::to_string(config.level_of_detail);
+
+        CHECK(fs::is_directory(data_root));
+
+        for (auto z = 0; z < chunks_in_z; ++z) {
+            const auto z_dir = data_root / std::to_string(z);
+            CHECK(fs::is_directory(z_dir));
+
+            for (auto y = 0; y < chunks_in_y; ++y) {
+                const auto y_dir = z_dir / std::to_string(y);
+                CHECK(fs::is_directory(y_dir));
+
+                for (auto x = 0; x < chunks_in_x; ++x) {
+                    const auto x_file = y_dir / std::to_string(x);
+                    CHECK(fs::is_regular_file(x_file));
+                    const auto file_size = fs::file_size(x_file);
+                    EXPECT_EQ(file_size, expected_file_size);
+                }
+
+                CHECK(
+                  !fs::is_regular_file(y_dir / std::to_string(chunks_in_x)));
+            }
+
+            CHECK(!fs::is_directory(z_dir / std::to_string(chunks_in_y)));
+        }
+
+        CHECK(!fs::is_directory(data_root / std::to_string(chunks_in_z)));
+
+        retval = 0;
+    } catch (const std::exception& exc) {
+        LOG_ERROR("Exception: %s\n", exc.what());
+    } catch (...) {
+        LOG_ERROR("Exception: (unknown)");
+    }
+
+    // cleanup
+    if (fs::exists(base_dir)) {
+        fs::remove_all(base_dir);
+    }
+    return retval;
+}
